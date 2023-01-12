0d7f14063780bb6ac5fe895c64aa4f0618294b4e
Ajay0-data <122398279+Ajay0-data@users.noreply.github.com>
2023-01-11 17:15:52 -0700

    Add files via upload

---
+++ MY IBM COURSE 2.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# <center>
+#     <img src="https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-DS0105EN-SkillsNetwork/labs/Module2/images/SN_web_lightmode.png" width="300" alt="cognitiveclass.ai logo"  />
+# </center>
+# 
+
+# #### Add your code below following the instructions given in the course
+# 
+
+# # My Jupyter Notebook on IBM Watson Studio
+
+# ## Ajibose Okanlawon
+
+# Data Scientist/Machine Learning Engineer
+
+# *I am interested in data science because the idea of making predictions using Artificial Intelligence tools has limitless possibilities*
+
+# ### Summation operation
+
+# In[3]:
+
+
+x = 1+1
+print(x)
+
+
+#  - a
+#  - b
+#  - c
+#  1. a
+#  2. b
+#  3. c
+#  
+#  *****
